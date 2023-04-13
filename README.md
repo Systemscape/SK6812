a0a9e1cc47ee8e9d6a77993651b62493d4f1ee39
JuliDi <20155974+JuliDi@users.noreply.github.com>
2023-04-13 12:55:18 +0200

    first attempty with DelayUs

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 embedded-hal driver for SK6812
 
 ## Datasheets
-- https://cdn-shop.adafruit.com/product-files/1138/SK6812+LED+datasheet+.pdf
+- https://cdn-shop.adafruit.com/product-files/1138/SK6812+LED+datasheet+.pdf (RGB)
+- https://cdn-shop.adafruit.com/product-files/2757/p2757_SK6812RGBW_REV01.pdf (RGBW)
 - Protocol guide/implementation notes: https://ecksteinimg.de/Datasheet/Pololu/ledstrip_for_sk6812_userguide.pdf
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+use embedded_hal::digital::OutputPin;
+use embedded_hal_async::delay::DelayUs;
+
+type RGBW = smart_leds::RGBW<u8>;
+
+pub struct Sk6812<Delay: DelayUs, Pin: OutputPin> {
+    delay: Delay,
+    pin: Pin,
 }
+
+impl<Delay: DelayUs, Pin: OutputPin> Sk6812<Delay, Pin> {
+    pub async fn new(delay: Delay, pin: Pin) -> Self {
+        Self { delay, pin }
+    }
+
+    pub async fn write(&mut self, iter: impl Iterator<Item = RGBW>) {
+        for RGBW { r, g, b, a } in iter {
+            self.write_byte(g);
+            self.write_byte(r);
+            self.write_byte(g);
+            self.write_byte(a.0);
+        }
+    }
+
+    async fn write_byte(&mut self, mut data: u8) {
+        for _ in 0..8 {
+            // If the MSB is 1 (i.e. masked byte non-zero), send the sequence for 1 and vice versa
+            if (data & 0x80) != 0 {
+                self.delay.delay_us(1);
+                self.pin.set_high().ok();
+                self.delay.delay_us(2); // Send long HIGH pulse
+                self.pin.set_low().ok();
+            } else {
+                self.delay.delay_us(1);
+                self.pin.set_high().ok();
+                self.delay.delay_us(1); // Send short HIGH pulse
+                self.pin.set_low().ok();
+                self.delay.delay_us(1);
+            }
+            // Shift 1 bit left so the next bit is the new MSB
+            data <<= 1;
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
