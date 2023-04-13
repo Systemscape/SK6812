db2b5875e81580bb4df6ea3ede349c8eeaa1b1a8
JuliDi <20155974+JuliDi@users.noreply.github.com>
2023-04-13 13:05:51 +0200

    add denies, add missing reset wait, add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
 #![no_std]
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+#![doc = include_str!("../README.md")]
 
 use embedded_hal::digital::OutputPin;
 use embedded_hal_async::delay::DelayUs;
 
 type RGBW = smart_leds::RGBW<u8>;
 
+/// SK6812 struct holding a `Delay` and the pin to which the LEDs are connected
 pub struct Sk6812<Delay: DelayUs, Pin: OutputPin> {
     delay: Delay,
     pin: Pin,
 }
 
 impl<Delay: DelayUs, Pin: OutputPin> Sk6812<Delay, Pin> {
+    /// Construct an instance from a [`Delay`] and [`Pin`]
     pub async fn new(delay: Delay, pin: Pin) -> Self {
         Self { delay, pin }
     }
 
+    /// Write the RGBW colors stored in `iter` to the LEDs
     pub async fn write(&mut self, iter: impl Iterator<Item = RGBW>) {
         for RGBW { r, g, b, a } in iter {
             self.write_byte(r).await;
@@ -22,8 +28,11 @@ impl<Delay: DelayUs, Pin: OutputPin> Sk6812<Delay, Pin> {
             self.write_byte(b).await;
             self.write_byte(a.0).await;
         }
+        // Send reset code after writing all bytes
+        self.delay.delay_us(90).await;
     }
 
+    /// Write a single byte to the LED according to the specification
     async fn write_byte(&mut self, mut data: u8) {
         for _ in 0..8 {
             // If the MSB is 1 (i.e. masked byte non-zero), send the sequence for 1 and vice versa
@@ -42,17 +51,5 @@ impl<Delay: DelayUs, Pin: OutputPin> Sk6812<Delay, Pin> {
             // Shift 1 bit left so the next bit is the new MSB
             data <<= 1;
         }
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
