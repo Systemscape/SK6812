9a3db5023433730d784d2e61483f3c64b7d8f4e8
JuliDi <20155974+JuliDi@users.noreply.github.com>
2023-04-13 12:58:48 +0200

    add missing .await s

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,10 @@ impl<Delay: DelayUs, Pin: OutputPin> Sk6812<Delay, Pin> {
 
     pub async fn write(&mut self, iter: impl Iterator<Item = RGBW>) {
         for RGBW { r, g, b, a } in iter {
-            self.write_byte(g);
-            self.write_byte(r);
-            self.write_byte(g);
-            self.write_byte(a.0);
+            self.write_byte(r).await;
+            self.write_byte(g).await;
+            self.write_byte(b).await;
+            self.write_byte(a.0).await;
         }
     }
 
@@ -28,16 +28,16 @@ impl<Delay: DelayUs, Pin: OutputPin> Sk6812<Delay, Pin> {
         for _ in 0..8 {
             // If the MSB is 1 (i.e. masked byte non-zero), send the sequence for 1 and vice versa
             if (data & 0x80) != 0 {
-                self.delay.delay_us(1);
+                self.delay.delay_us(1).await;
                 self.pin.set_high().ok();
-                self.delay.delay_us(2); // Send long HIGH pulse
+                self.delay.delay_us(2).await; // Send long HIGH pulse
                 self.pin.set_low().ok();
             } else {
-                self.delay.delay_us(1);
+                self.delay.delay_us(1).await;
                 self.pin.set_high().ok();
-                self.delay.delay_us(1); // Send short HIGH pulse
+                self.delay.delay_us(1).await; // Send short HIGH pulse
                 self.pin.set_low().ok();
-                self.delay.delay_us(1);
+                self.delay.delay_us(1).await;
             }
             // Shift 1 bit left so the next bit is the new MSB
             data <<= 1;
